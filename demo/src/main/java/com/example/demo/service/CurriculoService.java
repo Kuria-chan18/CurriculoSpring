39f77ca33184fc23ec534eb627439fb1b70e45d0
Maria Clara de Araújo Monteiro <113060036+Kuria-chan18@users.noreply.github.com>
2024-06-03 19:47:16 +0000

    Modificando.

--- demo/src/main/java/com/example/demo/service/CurriculoService.java
+++ demo/src/main/java/com/example/demo/service/CurriculoService.java
@@ -2,15 +2,11 @@ package com.example.demo.service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 import com.example.demo.model.Curriculo;
 import com.example.demo.repository.CurriculoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CurriculoService {
